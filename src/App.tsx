6a0519c55ef888ae9884256a0ce5827800d14d80
Andrew Davis <atd285@gmail.com>
2021-03-10 14:41:45 -0500

    finish app

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function StopwatchReducer(
     }
     case 'start': {
       return {
-        currentTime: 0,
+        ...state,
         isRunning: true,
         lastTime: Date.now(),
       };
@@ -36,22 +36,57 @@ function StopwatchReducer(
       };
     }
     case 'tick': {
+      if (!state.isRunning) return state;
       return {
         ...state,
         currentTime: state.currentTime + (Date.now() - state.lastTime),
+        lastTime: Date.now()
       };
     }
   }
 }
+
+function parseTime(
+  time: number
+): { hours: number; minutes: number; seconds: number; milliseconds: number } {
+  const date = new Date(time)
+  const hours = date.getHours() + date.getTimezoneOffset() / 60 - 24;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const milliseconds = date.getMilliseconds()
+  return {
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+  };
+}
+
 function App() {
   const [state, dispatch] = useReducer(StopwatchReducer, {
     isRunning: false,
     currentTime: 0,
     lastTime: 0,
   });
+  const time = parseTime(state.currentTime);
+  const { hours, minutes, seconds, milliseconds } = time;
+  useEffect(() => {
+    let frame: number;
+    function tick(): void {
+      dispatch({ type: 'tick' });
+      frame = requestAnimationFrame(tick);
+    }
+    frame = requestAnimationFrame(tick);
+    return () => cancelAnimationFrame(frame);
+  });
   return (
     <div className="bg-gray-900 text-white h-screen flex flex-col justify-center items-center">
-      <span className="text-6xl font-bold tabular-nums">00:00:00.000</span>
+      <span className="text-6xl font-bold tabular-nums">
+        {hours.toString().padStart(2, '0')}:
+        {minutes.toString().padStart(2, '0')}:
+        {seconds.toString().padStart(2, '0')}.
+        {milliseconds.toString().padStart(3, '0')}
+      </span>
       <div className="space-x-4">
         <button
           onClick={() => dispatch({ type: 'reset' })}
@@ -59,18 +94,21 @@ function App() {
         >
           Reset
         </button>
-        <button
-          onClick={() => dispatch({ type: 'reset' })}
-          className="bg-green-500 hover:bg-green-600 border-4 border-green-700 rounded-full w-16 h-16"
-        >
-          Start
-        </button>
-        <button
-          onClick={() => dispatch({ type: 'reset' })}
-          className="bg-red-500 hover:bg-red-600 border-4 border-red-700 rounded-full w-16 h-16"
-        >
-          Stop
-        </button>
+        {!state.isRunning ? (
+          <button
+            onClick={() => dispatch({ type: 'start' })}
+            className="bg-green-500 hover:bg-green-600 border-4 border-green-700 rounded-full w-16 h-16"
+          >
+            Start
+          </button>
+        ) : (
+          <button
+            onClick={() => dispatch({ type: 'stop' })}
+            className="bg-red-500 hover:bg-red-600 border-4 border-red-700 rounded-full w-16 h-16"
+          >
+            Stop
+          </button>
+        )}
       </div>
     </div>
   );
